10fb0b98d39789dd8b4725128c897c6033932261
wenzapen <329042633@qq.com>
2024-04-09 22:07:07 +0800

    limiter.go

--- go.mod
+++ go.mod
@@ -17,5 +17,6 @@ require (
 	github.com/stretchr/testify v1.9.0
 	go.uber.org/multierr v1.10.0 // indirect
 	go.uber.org/zap v1.27.0
+	golang.org/x/time v0.5.0
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 )
---
+++ limiter/limiter.go
@@ -0,0 +1,43 @@
+package limiter
+
+import (
+	"context"
+	"sort"
+	"time"
+
+	"golang.org/x/time/rate"
+)
+
+type RateLimiter interface {
+	Wait(context.Context) error
+	Limit() rate.Limit
+}
+
+func Per(eventCount int, duration time.Duration) rate.Limit {
+	return rate.Every(duration / time.Duration(eventCount))
+}
+
+type MultiLimiter struct {
+	limiters []RateLimiter
+}
+
+func Multi(limiters ...RateLimiter) *MultiLimiter {
+	byLimit := func(i, j int) bool {
+		return limiters[i].Limit() < limiters[j].Limit()
+	}
+	sort.Slice(limiters, byLimit)
+	return &MultiLimiter{limiters: limiters}
+}
+
+func (l *MultiLimiter) Limit() rate.Limit {
+	return l.limiters[0].Limit()
+}
+
+func (l *MultiLimiter) Wait(ctx context.Context) error {
+	for _, l := range l.limiters {
+		if err := l.Wait(ctx); err != nil {
+			return err
+		}
+	}
+	return nil
+}
