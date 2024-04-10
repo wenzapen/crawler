59a5b4d98fe27bc8131ae51299a8b0815be64aa0
wenzapen <329042633@qq.com>
2024-04-10 21:13:21 +0800

    v0.3.3 draft0.5

--- parse/doubanbook/book.go
+++ parse/doubanbook/book.go
@@ -2,20 +2,98 @@ package doubanbook
 
 import (
 	"regexp"
+	"strconv"
 
 	"github.com/wenzapen/crawler/spider"
+	"go.uber.org/zap"
 )
+
+var DoubanBookTask = &spider.Task{
+	Options: spider.Options{
+		Name: "douban_book_list",
+	},
+	Rule: spider.RuleTree{
+		Root: func() ([]*spider.Request, error) {
+			roots := []*spider.Request{
+				{
+					Priority: 1,
+					URL:      "https://book.douban.com",
+					Method:   "GET",
+					RuleName: "数据Tag",
+				},
+			}
+			return roots, nil
+		},
+		Trunk: map[string]*spider.Rule{
+			"数据Tag": {ParseFunc: ParseTag},
+			"书籍列表":  {ParseFunc: ParseBookList},
+			"书籍简介": {ParseFunc: ParseBookDetails,
+				ItemFields: []string{
+					"书名",
+					"作者",
+					"页数",
+					"出版社",
+					"得分",
+					"价格",
+					"简介",
+				}},
+		},
+	},
+}
 
 const regexpStr = `<a href="([^"]+)" class="tag">([^<]+)</a>`
 
 func ParseTag(ctx *spider.Context) (spider.ParseResult, error) {
+	re := regexp.MustCompile(regexpStr)
 
+	matches := re.FindAllSubmatch(ctx.Body, -1)
+	result := spider.ParseResult{}
+
+	for _, m := range matches {
+		result.Requests = append(
+			result.Requests, &spider.Request{
+				Method:   "GET",
+				Task:     ctx.Req.Task,
+				URL:      "https://book.douban.com" + string(m[1]),
+				Depth:    ctx.Req.Depth + 1,
+				RuleName: "书籍列表",
+			})
+	}
+
+	zap.S().Debugln("parse book tag,count:", len(result.Requests))
+	// 在添加limit之前，临时减少抓取数量,防止被服务器封禁
+	// result.Requests = result.Requests[:1]
+	return result, nil
 }
 
 const BooklistRe = `<a.*?href="([^"]+)" title="([^"]+)"`
 
 func ParseBookList(ctx *spider.Context) (spider.ParseResult, error) {
+	re := regexp.MustCompile(BooklistRe)
+	matches := re.FindAllSubmatch(ctx.Body, -1)
+	result := spider.ParseResult{}
 
+	for _, m := range matches {
+		req := &spider.Request{
+			Priority: 100,
+			Method:   "GET",
+			Task:     ctx.Req.Task,
+			URL:      string(m[1]),
+			Depth:    ctx.Req.Depth + 1,
+			RuleName: "书籍简介",
+		}
+		req.TmpData = &spider.Temp{}
+
+		if err := req.TmpData.Set("book_name", string(m[2])); err != nil {
+			zap.L().Error("Set TmpData failed", zap.Error(err))
+		}
+
+		result.Requests = append(result.Requests, req)
+	}
+
+	zap.S().Debugln("parse book list,count:", len(result.Requests))
+
+	return result, nil
 }
 
 var autoRe = regexp.MustCompile(`<span class="pl"> 作者</span>:[\d\D]*?<a.*?>([^<]+)</a>`)
@@ -26,7 +104,27 @@ var scoreRe = regexp.MustCompile(`<strong class="ll rating_num " property="v:ave
 var intoRe = regexp.MustCompile(`<div class="intro">[\d\D]*?<p>([^<]+)</p></div>`)
 
 func ParseBookDetails(ctx *spider.Context) (spider.ParseResult, error) {
+	bookName := ctx.Req.TmpData.Get("book_name")
+	page, _ := strconv.Atoi(ExtraString(ctx.Body, pageRe))
 
+	book := map[string]interface{}{
+		"书名":  bookName,
+		"作者":  ExtraString(ctx.Body, autoRe),
+		"页数":  page,
+		"出版社": ExtraString(ctx.Body, public),
+		"得分":  ExtraString(ctx.Body, scoreRe),
+		"价格":  ExtraString(ctx.Body, priceRe),
+		"简介":  ExtraString(ctx.Body, intoRe),
+	}
+	data := ctx.Output(book)
+
+	result := spider.ParseResult{
+		Items: []interface{}{data},
+	}
+
+	zap.S().Debugln("parse book detail", data)
+
+	return result, nil
 }
 
 func ExtraString(contents []byte, re *regexp.Regexp) string {
@@ -34,5 +132,5 @@ func ExtraString(contents []byte, re *regexp.Regexp) string {
 	if len(match) >= 2 {
 		return string(match[1])
 	}
-	return nil
+	return ""
 }
